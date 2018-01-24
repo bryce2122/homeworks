edcba82d580d45d86602ee65f16b5399ccb1f33d
bryce <sbw2122@columbia.edu>
2018-01-24 00:05:17 -0800

    REDUX_CYCLE

--- W7D2/todoList/frontend/reducers/selectors.js
+++
@@ -1,7 +0,0 @@
-
-
-const allTodos = (state) => {
-    let keys = Object.keys(state.todos);
-    return keys.map(objId => state.todos[objId]);
-};
-export default allTodos;
--- W7D2/todoList/frontend/util/unique_id.js
+++
@@ -1,3 +0,0 @@
-module.exports = function uniqueId() {
-  return new Date().getTime();
-};
